a3497b9e7a4c10574a60f2c4ee5f97140ebc6369
Abulfayz <abulfayzmamatqulov@gmail.com>
2025-09-29 16:28:20 +0500

    Upload web site file

---
+++ sayoxatchi.uz/js/modal.js
@@ -0,0 +1,89 @@
+let modalBtn = document.querySelector(".modal-btn");
+let modalBg = document.querySelector(".modal-bg");
+let modalClose = document.querySelector(".modal-close");
+
+modalBtn.addEventListener("click", function () {
+  modalBg.classList.add("bg-active");
+});
+
+modalClose.addEventListener("click", function () {
+  modalBg.classList.remove("bg-active");
+});
+
+// Zomin Modal
+let modalBtn1 = document.querySelector(".modal-btn1");
+let modalBg1 = document.querySelector(".modal-bg1");
+let modalClose1 = document.querySelector(".modal-close1");
+
+modalBtn1.addEventListener("click", function () {
+  modalBg1.classList.add("bg-active");
+});
+
+modalClose1.addEventListener("click", function () {
+  modalBg1.classList.remove("bg-active");
+});
+
+// Aydarkol Modal
+let modalBtn2 = document.querySelector(".modal-btn2");
+let modalBg2 = document.querySelector(".modal-bg2");
+let modalClose2 = document.querySelector(".modal-close2");
+
+modalBtn2.addEventListener("click", function () {
+  modalBg2.classList.add("bg-active");
+});
+
+modalClose2.addEventListener("click", function () {
+  modalBg2.classList.remove("bg-active");
+});
+
+// Amirsoy Modal
+let modalBtn3 = document.querySelector(".modal-btn3");
+let modalBg3 = document.querySelector(".modal-bg3");
+let modalClose3 = document.querySelector(".modal-close3");
+
+modalBtn3.addEventListener("click", function () {
+  modalBg3.classList.add("bg-active");
+});
+
+modalClose3.addEventListener("click", function () {
+  modalBg3.classList.remove("bg-active");
+});
+
+// BaxmalModal
+let modalBtn4 = document.querySelector(".modal-btn4");
+let modalBg4 = document.querySelector(".modal-bg4");
+let modalClose4 = document.querySelector(".modal-close4");
+
+modalBtn4.addEventListener("click", function () {
+  modalBg4.classList.add("bg-active");
+});
+
+modalClose4.addEventListener("click", function () {
+  modalBg4.classList.remove("bg-active");
+});
+
+// Temur Modal
+let modalBtn5 = document.querySelector(".modal-btn5");
+let modalBg5 = document.querySelector(".modal-bg5");
+let modalClose5 = document.querySelector(".modal-close5");
+
+modalBtn5.addEventListener("click", function () {
+  modalBg5.classList.add("bg-active");
+});
+
+modalClose5.addEventListener("click", function () {
+  modalBg5.classList.remove("bg-active");
+});
+
+// Said Modal
+let modalBtn6 = document.querySelector(".modal-btn6");
+let modalBg6 = document.querySelector(".modal-bg6");
+let modalClose6 = document.querySelector(".modal-close6");
+
+modalBtn6.addEventListener("click", function () {
+  modalBg6.classList.add("bg-active");
+});
+
+modalClose6.addEventListener("click", function () {
+  modalBg6.classList.remove("bg-active");
+});
